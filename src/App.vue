9385241720d814ae438b64b0c6cf2a549f2c14af
ZRMYDYCG <547471919@qq.com>
2025-06-12 17:10:17 +0800

    build 🚀: 项目路径别名配置

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div></div>
-</template>
+  <div>
+    <button>Change Message</button>
+  </div>
+</template>  
 
-<style scoped></style>
+<script setup lang="ts">
+  import { ref } from 'vue'
+  const message = ref('Hello Vue 3!')
+</script>   
+
+<style scoped>
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+import * as path from 'path'
 
-// https://vite.dev/config/
+// 配置文档: https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    tailwindcss(),
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '~': path.resolve(__dirname, 'src/assets')
+    }
+  }
 })

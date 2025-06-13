0d423002bc039d8905e7d45b79f401f471f774ef
ZRMYDYCG <547471919@qq.com>
2025-06-14 00:12:47 +0800

    feat: use a single-table query

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
-<script setup lang="ts">
+<script setup>
+import { onMounted, ref } from 'vue'
 
+import { supabase } from '../supabase/index.ts'
+
+const books = ref([])
+
+async function getBooks() {
+  const { data } = await supabase.from('book').select()
+
+  books.value = data
+}
+
+onMounted(() => {
+  getBooks()
+})
 </script>
 
 <template>
-  <div>
-    <div />
-  </div>
+  <ul>
+    <li v-for="book in books" :key="book.id">
+      {{ book.name }} （编号:{{ book.id }}，价格:{{ book.price }}）
+    </li>
+  </ul>
 </template>
-
-<style scoped>
-
-</style>

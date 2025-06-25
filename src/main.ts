c4022dc86eac5857f0133f7c5911bcae7dc95da2
ZRMYDYCG <547471919@qq.com>
2025-06-25 16:05:15 +0800

    style: scrollbar

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import '~/css/tailwind.css'
+import '~/css/global.css'
 // SVG插件配置
 import 'virtual:svg-icons-register'
 
